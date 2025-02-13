1a8934a8c3acf5b6e55c66b84a32ada8b88acf21
Deason <142931108+ZHHhang@users.noreply.github.com>
2025-02-13 01:52:56 -0700

    Update ATOA_Test.sh

--- ATOA/exp/test/ATOA_Test.sh
+++ ATOA/exp/test/ATOA_Test.sh
@@ -1,10 +1,12 @@
 cd ../../ 
-python test_3D.py \
+python neuralplanner_CrossEntropy_R3D_arb_new_loss_e2e.py \
+--MLP_path "./models/crossentropy/test/CNN_NEWLOSS_3D/test_use/MLP_E2E_mask_3d_epoch=200_N_10_Np_1800_DropMLP=0.20.pkl" \
+--CNN_path "./models/crossentropy/test/CNN_NEWLOSS_3D/test_use/CNN_E2E_mask_3d_epoch=200_N_10_Np_1800_DropMLP=0.20.pkl" \
 --input_size 66 --num_sector_theta 90 --num_sector_phi 180 \
---enviro_index_low 0 --enviro_index_up 2 \
---Path_index_low 2 --Path_index_up 4 \
---Plot_flag 1 --Path_lvc_state 1 \
---stuck_search_time 90 --Sparse_index 5
+--enviro_index_low 8 --enviro_index_up 9 \
+--Path_index_low 238 --Path_index_up 239 \
+--Plot_flag 1 --Path_lvc_state 0 \
+--stuck_search_time 1000 --Sparse_index 5
 cd exp
 
 #--scale_para 4 --bias 20.0  need to be control on top of the script
