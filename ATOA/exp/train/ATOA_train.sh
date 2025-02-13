e5adf7131368e0c07c4dd689e6949ddd8862b0d4
Deason <142931108+ZHHhang@users.noreply.github.com>
2025-02-13 01:56:37 -0700

    Update ATOA_train.sh

--- ATOA/exp/train/ATOA_train.sh
+++ ATOA/exp/train/ATOA_train.sh
@@ -4,7 +4,7 @@ python train.py --model_path ./models/crossentropy/test/CNN_NEWLOSS/dropout_0.2 
 --num_sector 180 --batch_size_train 100 \
 --learning_rate_together 0.0001 --learning_rate_encoder 0.00001 --learning_rate_planner 0.0001 \
 --interpolation_point_num 50 --dropout_p_MLP 0.2 --dropout_p_CNN 0.0 \
---load_data_N 35 --load_data_NP 200 \
+--load_data_N 30 --load_data_NP 200 \
 --T_data_N 10 --T_data_NP 40 --T_data_s 90 --T_data_sp 4000 \
 --UT_data_N 10 --UT_data_NP 40 --UT_data_s 100 --UT_data_sp 0 \
 --k1 1 --k2 4.0 --k3 0.1 --k4 2 \
